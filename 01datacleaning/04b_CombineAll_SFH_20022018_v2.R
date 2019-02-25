6ff78046f79533a3684159ebb5d55883bf5d503b
bhaskar-kamble <kbhaskar.iitk@gmail.com>
2019-02-25 18:41:40 +0100

    file 04b_CombineAl_SFH...txt updated

--- 01datacleaning/04b_CombineAll_SFH_20022018_v2.R
+++ 01datacleaning/04b_CombineAll_SFH_20022018_v2.R
@@ -32,5 +32,15 @@ features_SFH2018 <- c(
 
 SFH20022018 <- rbind(all_SFH_20022017[,features_SFH20022017] , file_with2018_SFH[,features_SFH2018])
 #remove duplicates
-
-save(SFH20022018,file="SFH20022018.RData")
+remove_duplicates_by <- c(
+  "gebaeude_baujahr",
+  "energietraeger",              
+  "abrechnungsjahr",
+  "gebaeude_nutzflaeche",
+  "verbrauch_gesamt_kwh",
+  "verbrauch_gesamt_kwh_spez",   
+  "Landkreis_von_GS"  
+)
+is_remove_duplicates_by <- which(names(SFH20022018) %in% remove_duplicates_by)
+SFH20022018 <- SFH20022018[!duplicated(SFH20022018[ , is_remove_duplicates_by]) , ]
+save(SFH20022018,file="SFH20022018_v2.RData")
